8e457316217505b9fc65a1126383ed85a42050ac
warapong-pj <warapong.pj@gmail.com>
2024-09-29 15:12:34 +0700

    add s3 terraform

---
+++ templates/s3/content/output.tf
@@ -0,0 +1,7 @@
+output "bucket_id" {
+  value = module.s3-bucket.s3_bucket_id
+}
+
+output "bucket_arn" {
+  value = module.s3-bucket.s3_bucket_arn
+}
---
+++ templates/s3/content/variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "project" {
+  default = "poc"
+}
+
+variable "environment" {
+  default = "DEV"
+}
+
+variable "name" {
+  default = "app1"
+}
+
+variable "acl" {
+  default = "private"
+}
+
+variable "owner" {
+  default = "platform-team"
+}
